a82ff4ac04ca25c6c459d7e088aee38e19fc0c4d
Isidro1234 <skillsizy@gmail.com>
2026-08-21 19:04:16 -0500

    novos

---
+++ app/user/student/TutoringLive/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Tutoring() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ app/user/visitant/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Visitant() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
