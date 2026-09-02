90f8f641f43b70e7cf80c9e2b5e77437280e4d67
Isidro1234 <skillsizy@gmail.com>
2026-09-02 09:21:41 -0500

    novo

--- components/structure/PersonalizedVideoPlayer.tsx
+++ components/structure/PersonalizedVideoPlayer.tsx
@@ -3,23 +3,6 @@ import React, { useRef } from 'react'
 
 export default function PersonalizedVideoPlayer() {
     const ref = useRef<HTMLVideoElement>(null)
-    const formater = new Intl.DurationFormat('video' , {
-        hours:'2-digit',
-        minutes:'2-digit',
-        seconds:'2-digit'
-    })
-    function handleVolume(){
-
-    }
-    function handlePlay(){
-
-    }
-    function handleSlide(){
-
-    }
-    function handleTiming(){
-
-    }
     
   return (
     <div>
