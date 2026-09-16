14cc013726ac60cedb5888b8f8a3838bc7f8f076
Isidro1234 <skillsizy@gmail.com>
2026-09-16 11:10:22 -0500

    novos

--- next.config.ts
+++ next.config.ts
@@ -60,7 +60,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
+async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule: any) =>
       rule.test?.test?.('.svg')
